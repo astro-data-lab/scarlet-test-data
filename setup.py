4f408f66c6cd73ab5c72566ecbee0e4d4261e846
Benjamin Remy <remy.benjamin.101@gmail.com>
2024-05-16 10:44:04 -0400

    pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='scarlet-test-data',
+    version='0.1.0',    
+    description='Scarlet, test data storage',
+    url='https://github.com/astro-data-lab/scarlet-test-data',
+    author='Peter Melchior',
+    author_email='melchior@astro.princeton.edu',
+    license='MIT',
+    packages=['scarlet_test_data'],
+)
