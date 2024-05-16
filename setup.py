0e8ebb8f248df1ca6c35e2d95ec4c0fdd8c6ae9a
Benjamin Remy <remy.benjamin.101@gmail.com>
2024-05-16 17:09:05 -0400

    clean setup

--- setup.py
+++ setup.py
@@ -1,39 +1,5 @@
 from setuptools import setup
 
-import os
-import importlib
-import pip
-
-module_name = "git"
-package = "gitpython"
-
-def install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
-
-try:
-    import git
-except ModuleNotFoundError:
-    install(package)
-finally:
-    globals()[module_name] = importlib.import_module(module_name)
-
-def pull_first():
-    """This script is in a git directory that can be pulled."""
-    cwd = os.getcwd()
-    gitdir = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(gitdir)
-    g = git.cmd.Git(gitdir)
-    try:
-        g.execute(['git', 'lfs', 'pull'])
-    except git.exc.GitCommandError:
-        raise RuntimeError("Make sure git-lfs is installed!")
-    os.chdir(cwd)
-
-pull_first()
-
 setup(
     name='scarlet-test-data',
     version='0.1.0',    
